234b372758e69aa49e33ac49ab79fe92c25c2bbf
BJ Ray <bjandraney@mac.com>
2008-12-03 04:09:24 +0000

    Initial load of the retorted client app.

---
+++ client/iphone/Retorted/Classes/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  Retorted
+//
+//  Created by B.J. Ray on 12/2/08.
+//  Copyright Forward Echo, LLC 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
---
+++ client/iphone/Retorted/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Retorted
+//
+//  Created by B.J. Ray on 12/2/08.
+//  Copyright Forward Echo, LLC 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
+
