7877756231c25662a9b979622eeaeb77ba3841a4
Jay Walker <mr_calamari@yahoo.com>
2008-12-09 20:28:36 +0000

    Initial upload of models for tag, retort, rating Update to migration script

--- site/db/migrate/001_create_attributions.rb
+++ site/db/migrate/001_create_attributions.rb
@@ -2,10 +2,7 @@ class CreateAttributions < ActiveRecord::Migration
   def self.up
     create_table :attributions do |t|
       t.string :who, :what, :where, :how
-      #t.string :what
       t.date :when
-      #t.string :where
-      #t.string :how
       t.timestamps
     end
   end
