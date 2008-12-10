bb59403e1c11d6dcf950a53c1b240c4c644f780f
BJ Ray <bjandraney@mac.com>
2008-12-10 22:36:59 +0000

    Added new initializers

--- client/iphone/Retorted/TRTag.h
+++ client/iphone/Retorted/TRTag.h
@@ -13,10 +13,14 @@
 	NSString *value;
 	NSInteger votes;			//used for tagCould...
 	NSInteger tagCloudValue;	//used for tagCloud...
+	NSInteger primaryKey;
 }
+
+- (id) initWithId:(NSInteger)aPrimaryKey Value:(NSString *) aValue;
+- (id)initWithId:(NSInteger)aPrimaryKey Value:(NSString *)aValue Votes:(NSInteger)voteCount TagCloudValue:(NSInteger)cloudValue;
 
 @property (nonatomic, retain) NSString *value;
 @property NSInteger votes;
 @property NSInteger tagCloudValue;
-
+@property NSInteger primaryKey;
 @end
--- client/iphone/Retorted/TRTag.m
+++ client/iphone/Retorted/TRTag.m
@@ -13,15 +13,31 @@
 @synthesize value;
 @synthesize votes;
 @synthesize tagCloudValue;
-
+@synthesize primaryKey;
 
 - (id) init {
+	return [self initWithId:-1 Value:@"" Votes:0 TagCloudValue:0];
+}
+
+- (id) initWithId:(NSInteger)aPrimaryKey Value:(NSString *) aValue {
+	
+	return [self initWithId:primaryKey Value:aValue Votes:0 TagCloudValue:0];
+}
+
+//Designated initializer
+- (id)initWithId:(NSInteger)aPrimaryKey Value:(NSString *)aValue Votes:(NSInteger)voteCount TagCloudValue:(NSInteger)cloudValue {
 	if (![super init])
 		return nil;
+	
+	self.primaryKey = aPrimaryKey;
+	self.value = aValue;
+	self.votes = voteCount;
+	self.tagCloudValue = cloudValue;
 	
 	return self;
 }
 
+ 
 - (void)dealloc {
 	[super dealloc];
 }
