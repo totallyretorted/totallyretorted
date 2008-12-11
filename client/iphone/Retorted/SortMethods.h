3a31298ea91b5840d3cd9ba8d27df0091b397731
Jay Walker <mr_calamari@yahoo.com>
2008-12-11 00:47:40 +0000

    added sort enum

---
+++ client/iphone/Retorted/SortMethods.h
@@ -0,0 +1,24 @@
+/*
+ *  SortMethods.h
+ *  Retorted
+ *
+ *  Created by Jay Walker on 12/10/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#import "TRTag.h"
+
+
+enum TagSortOrder {
+	VoteCountDesc = 0
+	, VoteCountAsc
+	, AlphaAsc
+	, AlphaDesc
+} var;
+
+
+NSInteger tagVoteSortAsc(id tagLeft, id tagRight, void *ignore);
+NSInteger tagVoteSortDesc(id tagLeft, id tagRight, void* ignore);
+NSInteger tagAlphaSortAsc(id tagLeft, id tagRight, void *reverse);
+NSInteger tagAlphaSortDesc(id tagLeft, id tagRight, void* ignore);
