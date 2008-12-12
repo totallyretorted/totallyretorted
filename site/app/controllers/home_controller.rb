4566f090c7124b50ccca4967e009bf3f87906e96
Adam Strickland <adam.strickland@gmail.com>
2008-12-12 20:17:51 +0000

    adding home controller skeleton

--- site/app/controllers/home_controller.rb
+++ site/app/controllers/home_controller.rb
@@ -1,2 +1,10 @@
 class HomeController < ApplicationController
+  def index
+    @retorts = Retort.find(:all, :limit => 5)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      #format.xml { render :xml => @retorts.to_xml }
+    end
+  end
 end
