81b5e776a3a602641515b751fada8d2d2e81b22a
Adam Strickland <adam.strickland@gmail.com>
2008-12-17 20:30:30 +0000

    adding attribution test

---
+++ client/iphone/Retorted/TRAttributionTest.h
@@ -0,0 +1,17 @@
+//
+//  TRAttributionTest.h
+//  Retorted
+//
+//  Created by Adam Strickland on 12/17/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "GTMSenTestCase.h"
+#import "TRAttribution.h"
+
+
+@interface TRAttributionTest : SenTestCase {
+
+}
+
+@end
---
+++ client/iphone/Retorted/TRAttributionTest.m
@@ -0,0 +1,14 @@
+//
+//  TRAttributionTest.m
+//  Retorted
+//
+//  Created by Adam Strickland on 12/17/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "TRAttributionTest.h"
+
+
+@implementation TRAttributionTest
+
+@end
