97d97e4d22ad6e005a88464a116bb5456d545588
Adam Strickland <adam.strickland@gmail.com>
2008-12-17 20:22:51 +0000

    finished parser tests

--- client/iphone/Retorted/TRRetortXMLParserTest.h
+++ client/iphone/Retorted/TRRetortXMLParserTest.h
@@ -9,13 +9,16 @@
 #import "GTMSenTestCase.h"
 #import "TRRetortXMLParser.h"
 #import "TRRetort.h"
+#import "TRTag.h"
+#import "TRRating.h"
+#import "TRAttribution.h"
 
 @interface TRRetortXMLParserTest : SenTestCase {
 @private TRRetortXMLParser*	subject;
 @private NSError *err;
 }
 
-- (void) testSuperSimpleParseXml;
-//- (void) testStandardXml;
+- (void) testParseSuperSimpleXml;
+- (void) testParseStandardXml;
 
 @end
--- client/iphone/Retorted/TRRetortXMLParserTest.m
+++ client/iphone/Retorted/TRRetortXMLParserTest.m
@@ -24,8 +24,9 @@
 	[subject release];
 }
 
-- (void) testSuperSimpleParseXml
+- (void) testParseSuperSimpleXml
 {
+	NSLog(@"Running testParseSuperSimpleXml test");
 	NSData* input = [NSData dataWithContentsOfFile:[[NSBundle bundleForClass:[self class]] pathForResource:@"TRRetortXMLParserTest_testSuperSimpleXML" ofType:@"xml"]];	
 	STAssertNil(err, nil);
 	[subject parseRetortXML:input parseError:&err];
@@ -37,8 +38,9 @@
 	STAssertEquals([retort.primaryId intValue], 999, nil);
 	STAssertEqualObjects(retort.content, @"This is a simple test", nil);}
 
-- (void) testStandardXml
+- (void) testParseStandardXml
 {
+	NSLog(@"Running testParseStandardXml test");
 	NSData* input = [NSData dataWithContentsOfFile:[[NSBundle bundleForClass:[self class]] pathForResource:@"TRRetortXMLParserTest_testStandardXml" ofType:@"xml"]];
 	STAssertNil(err, nil);
 	[subject parseRetortXML:input parseError:&err];
@@ -48,5 +50,22 @@
 	STAssertEqualObjects([NSNumber numberWithUnsignedInt:[results count]], [NSNumber numberWithUnsignedInt:5], nil);
 	TRRetort* retort = [results objectAtIndex:0];
 	STAssertEquals([retort.primaryId intValue], 5, nil); 
+	STAssertEqualObjects(retort.content, @"Hippies.They're everywhere. They wanna save the earth, but all they do is smoke pot and smell bad.", nil);
+
+	STAssertEqualObjects(retort.attribution.who, @"Eric Cartman", nil);
+	STAssertEqualObjects(retort.attribution.where, @"South Park", nil);
+
+	STAssertEquals(retort.rating.positive, 3022, nil);
+	STAssertEquals(retort.rating.negative, 3282, nil);
+	float rank = (3022.0 / (3022.0 + 3282.0));
+	STAssertEquals(retort.rating.rank, rank, @"Expected %f, found %f", rank, retort.rating.rank);
+	
+	NSMutableArray* tags = retort.tags;
+	STAssertNotNil(tags, nil);
+	STAssertEquals([tags count], (unsigned int) 2, nil);
+	TRTag* tag = [tags objectAtIndex:0];
+	STAssertEquals([tag.primaryId intValue], 1, nil);
+	STAssertEquals(tag.weight, 6, nil);
+	STAssertEqualObjects(tag.value, @"south_park", nil);
 }
 @end
