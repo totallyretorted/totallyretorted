0f306539b8716a7b34ec20158f8b033b2855825c
Adam Strickland <adam.strickland@gmail.com>
2008-12-17 22:30:04 +0000

    adding rating test

---
+++ client/iphone/Retorted/TRRatingTest.h
@@ -0,0 +1,16 @@
+//
+//  TRRatingTest.h
+//  Retorted
+//
+//  Created by Adam Strickland on 12/17/08.
+//  Copyright 2008 Forward Echo, LLC. All rights reserved.
+//
+
+#import "GTMSenTestCase.h"
+#import "TRRating.h"
+
+@interface TRRatingTest : SenTestCase {
+	TRRating* subject;
+}
+
+@end
---
+++ client/iphone/Retorted/TRRatingTest.m
@@ -0,0 +1,28 @@
+//
+//  TRRatingTest.m
+//  Retorted
+//
+//  Created by Adam Strickland on 12/17/08.
+//  Copyright 2008 Forward Echo, LLC. All rights reserved.
+//
+
+#import "TRRatingTest.h"
+
+
+@implementation TRRatingTest
+
+- (void) setUp
+{
+	
+}
+
+- (void) tearDown
+{
+	
+}
+
+- (void) testInitWithPositiveNegativeRank
+{
+	
+}
+@end
