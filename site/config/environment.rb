bb1cca9943bf0a2c4f897f3c5cb482bc93434f56
Adam Strickland <adam.strickland@gmail.com>
2009-01-10 17:00:45 +0000

    resolved conflicts

--- site/config/environment.rb
+++ site/config/environment.rb
@@ -25,7 +25,7 @@ Rails::Initializer.run do |config|
   # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
   # config.gem "bj"
   config.gem "rcov"
-  config.gem "hpricot", :version => '~> 0.6'
+  #config.gem "hpricot"
   config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'mislav-will_paginate', :version => '~> 2.3.2', :lib => 'will_paginate', :source => 'http://gems.github.com'
@@ -36,6 +36,7 @@ Rails::Initializer.run do |config|
   #config.gem 'webrat'
   config.gem 'fastercsv'
   config.gem "ambethia-smtp-tls", :lib => "smtp-tls", :source => "http://gems.github.com/"
+
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
@@ -95,6 +96,6 @@ Rails::Initializer.run do |config|
 end
 
 #require 'lib/unmarshal.rb'
-#require 'rubygems'
-#gem 'hpricot', '>= 0.6'
-#require 'hpricot'
+require 'rubygems'
+gem 'hpricot', '>= 0.6'
+require 'hpricot'
