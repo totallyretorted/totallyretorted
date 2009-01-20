1f9935659716e6cb5d8076bf05dabbc12f7acbbc
Adam Strickland <adam.strickland@gmail.com>
2009-01-20 17:46:10 +0000

    adding spin script

--- site/config/deploy.rb
+++ site/config/deploy.rb
@@ -1,15 +1,23 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# added to work around 'stdin: is not a tty' error
+default_run_options[:pty] = true
+
+set :application, "retorted"
+set :repository,  "https://development.forwardecho.com/repos/retorted"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/retorted/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "totallyretorted.com"
+role :web, "totallyretorted.com"
+role :db,  "totallyretorted.com", :primary => true
+
+set :user, "retorted"
+set :scm_username, "capistrano"
+set :use_sudo, false
