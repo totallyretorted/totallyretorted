52524a2863a1903a4bb794365c5ca5d8335df1d1
Adam Strickland <adam.strickland@gmail.com>
2009-01-23 04:41:34 +0000

    slicehost

--- site/config/deploy.rb
+++ site/config/deploy.rb
@@ -2,13 +2,14 @@
 default_run_options[:pty] = true
 
 set :application, "retorted"
-set :repository,  "https://development.forwardecho.com/repos/retorted"
+set :repository,  "https://development.forwardecho.com/repos/retorted/trunk/site/"
+#set :repository,  "https://development.forwardecho.com/repos/retorted/tags/0.1.1/site/"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
-set :deploy_to, "/home/retorted/#{application}"
+set :deploy_to, "/home/retorted/railsapp"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
@@ -20,4 +21,5 @@ role :db,  "totallyretorted.com", :primary => true
 
 set :user, "retorted"
 set :scm_username, "capistrano"
+set :scm_password, "capistrano"
 set :use_sudo, false
