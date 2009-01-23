e3f1289e0673885583c5f149086c408c9449e255
Adam Strickland <adam.strickland@gmail.com>
2009-01-23 05:52:52 +0000

    changed content to string, length of 5000

--- site/db/migrate/20081209201842_create_retorts.rb
+++ site/db/migrate/20081209201842_create_retorts.rb
@@ -1,7 +1,7 @@
 class CreateRetorts < ActiveRecord::Migration
   def self.up
     create_table :retorts do |t|
-      t.text :content      
+      t.string :content, :length => 5000      
       t.timestamps
       t.references :attribution
     end
