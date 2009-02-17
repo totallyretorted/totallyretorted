7c30ebfe9191e003a2cce2dc3ce90d9a05c1f6c7
Adam Strickland <adam.strickland@gmail.com>
2009-02-17 15:01:59 +0000

    gutted load_retort_data migration

--- site/db/migrate/20090125225831_load_retort_data.rb
+++ site/db/migrate/20090125225831_load_retort_data.rb
@@ -10,25 +10,25 @@ end
 
 class LoadRetortData < ActiveRecord::Migration
   def self.up
-    clear_data
+    #clear_data
         
-    FasterCSV.foreach(File.join(File.dirname(__FILE__), "..", "data", ENV['RAILS_ENV'] || 'development', 'retorts.csv'), {:headers => :first_row}) do |row|
-      retort = Retort.new(:content => row["retort"])
-      row["tags"].split(/,\s*/).each do |tag|
-        retort.tags << Tag.find_or_create_by_value(tag)
-      end
-      retort.rating = Rating.new(:positive => 1)
-      if(row["who"] or row["what"] or row["where"] or row["when"] or row["how"])
-        a = Attribution.new
-        a.who = row["who"]
-        a.when = row["when"]
-        a.what = row["what"]
-        a.where = row["where"]
-        a.how = row["how"]
-        retort.attribution = a
-      end
-      retort.save
-    end
+    #FasterCSV.foreach(File.join(File.dirname(__FILE__), "..", "data", ENV['RAILS_ENV'] || 'development', 'retorts.csv'), {:headers => :first_row}) do |row|
+      #retort = Retort.new(:content => row["retort"])
+      #row["tags"].split(/,\s*/).each do |tag|
+        #retort.tags << Tag.find_or_create_by_value(tag)
+      #end
+      #retort.rating = Rating.new(:positive => 1)
+      #if(row["who"] or row["what"] or row["where"] or row["when"] or row["how"])
+        #a = Attribution.new
+        #a.who = row["who"]
+        #a.when = row["when"]
+        #a.what = row["what"]
+        #a.where = row["where"]
+        #a.how = row["how"]
+        #retort.attribution = a
+      #end
+      #retort.save
+    #end
   end
 
   def self.down
